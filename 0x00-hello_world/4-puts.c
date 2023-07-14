b535c419472a97f6d56d4190b676ea37bb728ce6
Kojo Otabil <breakages@gmail.com>
2023-07-13 17:12:55 -0700

    Fixed the Question 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+* main - This is the stating point of the program
+* @void - This means the main function takes no arguments
+* Return: This is an exit code to terminate the program
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
