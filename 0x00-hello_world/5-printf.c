ed2e2939ac7553e0efd3a626811608753f327888
Kojo Otabil <breakages@gmail.com>
2023-07-13 17:18:26 -0700

    Question 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is the stating point
+ * Return: This is the termination of the program
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
