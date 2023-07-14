9391dabb1517d182052e7ef3e4a0123e9d74fe93
Kojo Otabil <breakages@gmail.com>
2023-07-13 18:18:22 -0700

    Question 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - Program Entry Point
+* Return: Program Exit
+*/
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
+
