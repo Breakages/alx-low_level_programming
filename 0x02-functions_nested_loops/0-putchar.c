eaecf65624d94b82c6a229c0b2a1971d1c7d9415
Kojo Otabil <breakages@gmail.com>
2023-07-18 14:42:43 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - Program Starts Here
+* Description: 'this prints putchar to stdout'
+* Return: Program Ends Here
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return(0);
+}
